2f1f0d55901da94f8c36b877bdfc678ad8111fbf
devld <devlduio@gmail.com>
2019-06-02 17:04:53 +0800

    change log format

--- src/main/java/me/devld/tour/controller/RequestLogAop.java
+++ src/main/java/me/devld/tour/controller/RequestLogAop.java
@@ -7,10 +7,12 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @Aspect
 @Component
@@ -23,10 +25,19 @@ public class RequestLogAop {
 
         HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String method = req.getMethod();
-        String uri = req.getRequestURI();
+        String url = req.getRequestURI();
+        String query = req.getQueryString();
+        if (query != null) {
+            url += "?" + query;
+        }
         String api = request.value();
         Long userId = SecurityUtil.userIdOrNull();
         String remoteAddr = req.getRemoteAddr();
+
+        String forwardFor = req.getHeader("X-Forwarded-For");
+        if (!StringUtils.isEmpty(forwardFor)) {
+            remoteAddr = forwardFor;
+        }
 
         long t = -1;
         Object result = null;
@@ -41,8 +52,8 @@ public class RequestLogAop {
         }
 
         LOGGER.info(
-                "{}({}) {} {} {} {}ms {}",
-                remoteAddr, userId, api, method, uri, t,
+                "{} {}({}) {} {} {} {}ms {}",
+                Instant.now().toString(), remoteAddr, userId, api, method, url, t,
                 throwable == null ? "success" : (throwable.getClass().getName() + ":" + throwable.getMessage())
         );
 
